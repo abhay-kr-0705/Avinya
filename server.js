81de983bb4a7d9c60a2b539290131fbe0e2a4eb3
Abhay Kumar <abhayk7481@gmail.com>
2025-02-02 00:36:59 +0530

    feat: add notification system with FCMljgoihnubounb  goiubg

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: ['https://genx-developers-club.netlify.app', 'http://localhost:5173'],
+  origin: ['https://genx-developers-club.netlify.app', 'http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
